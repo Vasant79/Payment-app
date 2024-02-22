2eb04b7a379b84ae4915193a75233796fa199f99
Vasant Singh Negi <Vasantnegi7@gmail.com>
2024-02-22 23:08:42 +0530

    landing page css 0.0.1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import { Link, Route, Router } from "react-router-dom";
-import Button from "./components/reusableComp/Button";
 
 function App() {
   return (
-    <div>
-      <Link to={"/signin"}>Signin</Link>
-      <br />
-      <Link to={"/signup"}>Signup</Link>
-      <br />
+    <div className="h-screen flex flex-col justify-center items-center">
+      <div className="shadow-md rounded-md p-3  px-6 mb-4 bg-white hover:bg-blue-100 transition duration-300">
+        <Link
+          to={"/signin"}
+          className="text-blue-600 font-semibold hover:text-blue-800"
+        >
+          Sign in
+        </Link>
+      </div>
+      <div className="shadow-md rounded-md p-3  px-6 mb-4 bg-white hover:bg-blue-100 transition duration-300">
+        <Link
+          to={"/signup"}
+          className="text-blue-600 font-semibold hover:text-blue-800"
+        >
+          Sign up
+        </Link>
+      </div>
     </div>
   );
 }
